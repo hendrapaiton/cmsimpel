25afbf74fae56ec653da98139e0a937770af15b2
Hendra Dwi Saputra <hendra@usa.com>
2025-11-29 08:22:44 +0700

    First commit

---
+++ src/main/java/website/garapan/cmsimpel/CmsimpelApplication.java
@@ -0,0 +1,13 @@
+package website.garapan.cmsimpel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsimpelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsimpelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/website/garapan/cmsimpel/CmsimpelApplicationTests.java
@@ -0,0 +1,13 @@
+package website.garapan.cmsimpel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsimpelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
